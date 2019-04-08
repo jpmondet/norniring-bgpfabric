cfc72885aea5db657bab20ec03ee77e1993ad770
jpmondet <jp@mondet.org>
2019-04-08 20:32:27 +0200

    Improving the script to prepare the environment

--- resources/add-compatible-python.sh
+++ resources/add-compatible-python.sh
@@ -4,19 +4,29 @@
 # (which is max 3.4 by default and Nornir needs at least 3.6)
 
 curl https://pyenv.run | bash
+wait
 echo 'export PATH="~/.pyenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
-source ~/.bashrc
-sudo apt-get update; sudo apt-get install -y \ 
-  make build-essential libssl-dev zlib1g-dev \ 
-  libbz2-dev libreadline-dev libsqlite3-dev wget \ 
-  curl llvm libncurses5-dev xz-utils tk-dev \ 
-  libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-pyenv install 3.7.3
+. ~/.bashrc
+wait
+if grep -q "stable" /etc/apt/apt.conf.d/00local
+then
+  sed -i 's/stable/jessie/g' /etc/apt/apt.conf.d/00local
+fi
+wait
+sudo apt-get update; sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev python-openssl python3-openssl
+wait
+pyenv install 3.6.3
+wait
 git clone https://github.com/jpmondet/norniring-bgpfabric
+wait
 cd norniring-bgpfabric
-pyenv local 3.7.3
+wait
+pyenv local 3.6.3
+wait
 python -m venv venv
-source venv/bin/activate
+wait
+. venv/bin/activate
+wait
 pip install -r requirements.txt
